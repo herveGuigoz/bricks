bf79c990f3c0079ed1c091b83e3d195695b3919e
herveGuigoz <herve.guigoz@gmail.com>
2024-10-04 12:59:10 +0200

    ✨ feat: fastapi

--- refine/ressource/__brick__/{{name}}/index.ts
+++
@@ -1,4 +0,0 @@
-export { {{name.pascalCase()}}List } from "./list";
-export { {{name.pascalCase()}}Create } from "./create";
-export { {{name.pascalCase()}}Edit } from "./edit";
-export { {{name.pascalCase()}}Show } from "./show";
--- refine/ressource/__brick__/{{name}}/list.tsx
+++
@@ -1,6 +0,0 @@
-import { IResourceComponentsProps } from "@refinedev/core";
-import { AntdListInferencer } from "@refinedev/inferencer/antd";
-
-export const {{name.pascalCase()}}List: React.FC<IResourceComponentsProps> = () => {
-  return <AntdListInferencer />;
-};
